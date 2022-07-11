a7b82bc8502dfac8bbfc9702cf915ea94ef21df5
ZombiesThe <florenciazanoni1@gmail.com>
2022-07-10 21:19:39 -0300

    agrego idBuscado

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 //SIMULADOR DE COMPRAS
 
-
 //STOCK DE LIBROS
 const libros = [
   {
@@ -40,7 +39,7 @@ class Carrito {
   }
 
   hayStock(libro, unidades) {
-    if (libro.stock != 0 && libro.stock>= unidades) {
+    if (libro.stock != 0 && libro.stock >= unidades) {
       return true;
     }
     if (libro.stock == 0) {
@@ -54,21 +53,23 @@ class Carrito {
   }
 
   agregarItem(libro, unidades) {
-    if (this.hayStock(libro, unidades) && this.productos.find((el)=>el.id == libro.id) == null) {
-      libro.stock-=unidades; 
-      libro.cantidad_comprada = unidades; // AGREGO CANTIDAD COMPRADA  
-      this.productos.push(libro);    
+    if (
+      this.hayStock(libro, unidades) &&
+      this.productos.find((el) => el.id == libro.id) == null
+    ) {
+      libro.stock -= unidades;
+      libro.cantidad_comprada = unidades; // AGREGO CANTIDAD COMPRADA
+      this.productos.push(libro);
     } else {
       return false;
     }
   }
-
 }
 
 // USUARIOS REGISTRADOS
-const user01 = new Usuario("Florencia", "florencia@mail.com", "12345"); 
+const user01 = new Usuario("Florencia", "florencia@mail.com", "12345");
 const user02 = new Usuario("Kevin", "kevin@mail.com", "abcde");
-const user03 = new Usuario("Nicole", "nicole@mail.com", "00000"); 
+const user03 = new Usuario("Nicole", "nicole@mail.com", "00000");
 
 // USUARIO NUEVO
 const user04 = new Usuario(
@@ -77,24 +78,26 @@ const user04 = new Usuario(
   prompt("Elija su contraseña")
 );
 
-function preguntarID(){
-  return prompt("Ingrese el ID del libro que desea agregar - 01 Vermeer, 02 Libro de Matematicas, 03 Bajo La misma Estrella")
+function preguntarID() {
+  return prompt(
+    "Ingrese el ID del libro que desea agregar - 01 Vermeer, 02 Libro de Matematicas, 03 Bajo La misma Estrella"
+  );
 }
-function preguntarUnidades(){
-  return prompt("Ingrese cuantos libros del producto desea")
+function preguntarUnidades() {
+  return prompt("Ingrese cuantos libros del producto desea");
 }
 
 let respuesta = prompt("Desea comprar? (si/no)");
-const carrito01 = new Carrito()
+const carrito01 = new Carrito();
 
-while(respuesta != "no") {
-
-  const elementoLibro = libros.find((elemento) => elemento.id == preguntarID());
+while (respuesta != "no") {
+  const idBuscado = preguntarID();
+  const elementoLibro = libros.find((elemento) => elemento.id == idBuscado);
 
   //AGREGO EL LIBRO Y LA CANTIDAD ESPECIFICADA
   carrito01.agregarItem(elementoLibro, preguntarUnidades());
 
-  respuesta = prompt("Desea seguir comprando? (si/no)")
+  respuesta = prompt("Desea seguir comprando? (si/no)");
 }
 precioFinal(carrito01);
 
@@ -115,5 +118,3 @@ function precioFinal(carrito) {
   console.log("Total " + total);
   console.log("Total con envio + IVA = " + precioFinal);
 }
-
-
